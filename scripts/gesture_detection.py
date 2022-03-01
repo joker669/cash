8aa8855ebf77573b55fd027ad9a615d00fa37fd0
alexli1998 <67549026+alexli1998@users.noreply.github.com>
2022-03-01 12:42:14 +0800

    implemented gesture detection

--- scripts/gesture_detection.py
+++ scripts/gesture_detection.py
@@ -3,63 +3,87 @@
 
 import rospy
 import numpy as np
+import cv2
 from cash.msg import gesture_info
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import cv2
+from cvzone.HandTrackingModule import HandDetector
 
-def get_coordinates(cv_img):
-    '''
-    Plz modify this function to call your algorithms to get the coordinates
-    :param cv_img:the image frame
-    :return: a four-elements integer list which represents the coordinates of the target (gesture) in the frame
-    '''
-    x_upleft = 20
-    y_upleft = 40
-    rectangle_height = 30
-    rectangle_width = 30
-    return [x_upleft, y_upleft, rectangle_height, rectangle_width]
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+
+def get_gesture_class(finger_status):
+    # determined by the number of fingers are up
+    class_no = 0
+
+    for finger in finger_status:
+        if finger == 1:
+            class_no += 1
+
+    return class_no
+
+
+def process_hand(cv_img):
+    bbox = None
+    gesture_class = None
+    hands = detector.findHands(cv_img, draw=False)
+
+    if not hands:
+        print('WARNING: no hand detected!')
+        return bbox, gesture_class  # return None, None
+
+    for hand in hands:
+        if hand1["type"] != 'Right':  # control only with right hand
+            continue
+
+        # get bbox
+        bbox = list(hand["bbox"])
+        # get gesture class
+        finger_status = detector.fingersUp(hand)
+        gesture_class = get_gesture_class(finger_status)
+
+    if not bbox:
+        print('WARNING: please use right hand for controlling!')  # return None, None
+
+    return bbox, gesture_class
 
 
 def callback(data):
-    global bridge, count,face_pub
-    
+    global bridge, count, face_pub
+
     # get the frame info
     cv_img = bridge.imgmsg_to_cv2(data, "bgr8")
     count += 1
-    print('receieved: frame',count )
-    #cv2.imshow("frame", cv_img)
-    #cv2.waitKey(1)
-    
-    #Below are just some mock codes. Plz modify the code. 
-    width = 640  #the image width ( corresponding to the coordinates)
-    height = 480  #the image height ( corresponding to the coordinates)
-    gesture = 1 #call your algorithms to get which kind of gesture
-    co = get_coordinates(cv_img)
-    
+    print('received: frame ', count)
+    # cv2.imshow("frame", cv_img)
+    # cv2.waitKey(1)
+
+    height, width, _ = frame.shape
+    bbox, gesture = process_hand(cv_img)
+
     fi = gesture_info()
-    fi.width = width
     fi.height = height
+    fi.width = width
+    fi.co = bbox
     fi.gesture = gesture
-    fi.co = co
-    
+
     rospy.loginfo(fi)
     face_pub.publish(fi)
 
 
-def  subscriber():
-    rospy.init_node('gesture_dection_node', anonymous=True)
+def subscriber():
+    rospy.init_node('gesture_detection_node', anonymous=True)
     rospy.Subscriber('/image_view/image_raw', Image, callback)
     rospy.spin()
-    
-    
+
+
 if __name__ == '__main__':
     try:
         count = 0
         bridge = CvBridge()
-        face_pub=rospy.Publisher('gesture_info', gesture_info, queue_size = 1) 
+        face_pub = rospy.Publisher('gesture_info', gesture_info, queue_size=1)
         subscriber()
     except rospy.ROSInterruptException as E:
-        print("quit unsuccessfully due  to ",E)
+        print("quit unsuccessfully due  to ", E)
 
 
