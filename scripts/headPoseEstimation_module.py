0b59a25efae5f5aafef63bb3f093c61bff630505
GuXincheng <86959867+GuXincheng@users.noreply.github.com>
2022-03-12 00:48:03 +0800

    Update headPoseEstimation_module.py
    FaceMesh_modeule.py & headPoseEstimation_module.py.  The 2 files are required for face_dectection_Gu.py to import.

--- scripts/headPoseEstimation_module.py
+++ scripts/headPoseEstimation_module.py
@@ -5,11 +5,12 @@ import math
 import cv2
 import numpy as np
 
-def headPoseEstimation(image, face_2d, face_3d, nose_2d, nose_3d):
+
+def headPoseEstimation(image, keypoint_2d, keypoint_3d, nose_2d, nose_3d):
     # Convert it to the NumPy array
-    face_2d = np.array(face_2d, dtype=np.float64)
+    keypoint_2d = np.array(keypoint_2d, dtype=np.float64)
     # Convert it to the NumPy array
-    face_3d = np.array(face_3d, dtype=np.float64)
+    keypoint_3d = np.array(keypoint_3d, dtype=np.float64)
 
     ih, iw, ic = image.shape
     # The camera matrix
@@ -23,7 +24,7 @@ def headPoseEstimation(image, face_2d, face_3d, nose_2d, nose_3d):
     dist_matrix = np.zeros((4, 1), dtype=np.float64)
 
     # Solve PnP
-    success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+    success, rot_vec, trans_vec = cv2.solvePnP(keypoint_3d, keypoint_2d, cam_matrix, dist_matrix)
     # print("Rotation Vector:\n {0}".format(rot_vec))
     # print("Translation Vector:\n {0}".format(trans_vec))
 
@@ -54,10 +55,11 @@ def headPoseEstimation(image, face_2d, face_3d, nose_2d, nose_3d):
 
     # In[ ]:
     # Display the nose direction
-    nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix, cv2.cv2.SOLVEPNP_ITERATIVE)
-    p1 = (int(nose_2d[0]), int(nose_2d[1]))
-    p2 = (int(nose_3d_projection[0][0][0]), int(nose_3d_projection[0][0][1]))
-    cv2.line(image, p1, p2, (0, 255, 0), 3)  # Green
+    # nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix,
+    #                                                  cv2.cv2.SOLVEPNP_ITERATIVE)
+    # p1 = (int(nose_2d[0]), int(nose_2d[1]))
+    # p2 = (int(nose_3d_projection[0][0][0]), int(nose_3d_projection[0][0][1]))
+    # cv2.line(image, p1, p2, (0, 255, 0), 3)  # Green
 
     # Add the text on the image
     cv2.putText(image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
