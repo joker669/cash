b3ef520a58bd2029fb108179be9d6a36d3fdc3d5
aceirus <aceirus@hotmail.com>
2022-04-09 18:54:00 +0800

    fixed face detection module error of not returning any values

--- scripts/pose_alert.py
+++ scripts/pose_alert.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
                 if s > 2*24:
                     text = "Warning: Bad pose"
                     cv2.putText(image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                    cv2.rectangle(image, (5, 5), (iw - 5, ih - 5), (0, 0, 255), 2)
+                    #cv2.rectangle(image, (5, 5), (iw - 5, ih - 5), (0, 0, 255), 2)
                     # os.system("say 'Warning: Bad pose'")
                     playsound('warning.mp3',True)
             else:
